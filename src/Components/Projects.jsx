eb2b9fce122237d9d2b32b39c71c55b5a239ffa0
asfour <asfour@m.insta.kim>
2024-01-14 17:57:24 +0600

    update

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,7 +5,7 @@ function Projects() {
   return (
     <>
       <div id="Projects" className=" p-20 flex flex-col items-center justify-center">
-        <h1 data-aos="fade-right" className=" text-[52px] font-semibold mb-20 text-fuchsia-800 uppercase leading-normal">Projects</h1>
+        <h1 data-aos="fade-right" className=" text-[52px] font-semibold mb-20 text-fuchsia-500 uppercase leading-normal">Projects</h1>
         <div className=" grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-around gap-10">
               <img data-aos="fade-down" src={img4} height={250} width={250} className=' flex items-center justify-center rounded-3xl p-2 border-2 border-fuchsia-800  b_glow'/>
               <img data-aos="fade-up" src={img3} height={250} width={250} className=' flex items-center justify-center rounded-3xl p-2 border-2 border-fuchsia-800  b_glow'/>
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -4,7 +4,7 @@ function Services() {
   return (
     <>
       <div id="Services" className=" p-20 flex flex-col items-center justify-center">
-        <h1 data-aos="fade-right" className=" text-[52px] font-semibold mb-20 text-fuchsia-800 uppercase leading-normal">Services</h1>
+        <h1 data-aos="fade-right" className=" text-[52px] font-semibold mb-20 text-fuchsia-500 uppercase leading-normal">Services</h1>
         <div className=" grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-around gap-20">
               <h2 data-aos="fade-up" className=" text-fuchsia-800 text-[30px] flex border-2 border-fuchsia-800 justify-center items-center font-semibold rounded-ss-3xl rounded-br-3xl h-36 w-44 b_glow">HTML</h2>
               <h2 data-aos="fade-down" className=" text-fuchsia-800 text-[30px] flex border-2 border-fuchsia-800 justify-center items-center font-semibold rounded-ss-3xl rounded-br-3xl h-36 w-44 b_glow">CSS</h2>
